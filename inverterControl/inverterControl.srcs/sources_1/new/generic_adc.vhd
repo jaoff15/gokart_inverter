9eaaee691537647eacc8b8d779693067897d304f
Jonas <jonho12@student.sdu.dk>
2019-02-14 09:51:32 +0100

    ADC module added

---
+++ inverterControl/inverterControl.srcs/sources_1/new/generic_adc.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity generic_adc is
+    Port ( inp : in STD_LOGIC);
+end generic_adc;
+
+architecture Behavioral of generic_adc is
+
+begin
+
+
+end Behavioral;
