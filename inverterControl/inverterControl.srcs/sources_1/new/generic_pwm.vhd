6220f9c7fac278fa4c98885eb21598f9346e75ef
Jacob Offersen <jaoff15@student.sdu.dk>
2019-02-17 13:45:06 +0100

    Interface to pwm module changed

--- inverterControl/inverterControl.srcs/sources_1/new/generic_pwm.vhd
+++ inverterControl/inverterControl.srcs/sources_1/new/generic_pwm.vhd
@@ -9,47 +9,55 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity generic_pwm is
     Port ( clk            : in  STD_LOGIC;
-           freq           : in  STD_LOGIC_VECTOR(3 downto 0);
-           threshold_high : in  signed(31 downto 0);
-           threshold_low  : in  signed(31 downto 0);
-          -- phase          : in  STD_LOGIC;
+           clk_freq 	  : in  signed(31 downto 0);
+		   duty_cycle 	  : in  signed(31 downto 0);
+           phase          : in  signed(2 downto 0);	-- 0 = 0degrees, 1 = 120 degrees, 2 = 240 degrees
            pwm_high       : out STD_LOGIC;
            pwm_low        : out STD_LOGIC);
 end generic_pwm;
 
 architecture Behavioral of generic_pwm is
     -- Clock frequency
-    constant CLOCK_FREQ : signed(31 downto 0) := x"07735940"; -- 125000000kHz
+    --constant CLOCK_FREQ : signed(31 downto 0) := x"07735940"; -- 125000000kHz
 
     -- Counting directions
-    constant UP   : STD_LOGIC := '1';
     constant DOWN : STD_LOGIC := '0';
+	constant UP   : STD_LOGIC := '1';
     
     -- Output levels
     constant HIGH : STD_LOGIC := '1';
     constant LOW  : STD_LOGIC := '0';
+	
+	-- Deadtime
+	constant DEADTIME  : signed(31 downto 0) := x"00000005"; 
     
     -- Counter limits. When the count reaches the limit. The counting direction is changed.
-    constant COUNT_MAX : signed(31 downto 0) := x"07735940";
+    signal COUNT_MAX : signed(31 downto 0) := x"07735940";
     constant COUNT_MIN : signed(31 downto 0) := x"00000000";
     
     -- Counting direction (up or down)
-    signal dir : STD_LOGIC := UP;
+    signal dir : STD_LOGIC := NONE_INITIALIZED;
     
     -- Counter amount. The amount by which the counter should increase
     signal counter_amount : signed(31 downto 0) := x"00000000";
     
     -- Counter that counts on every clock pulse 
-    signal counter : signed(31 downto 0) := x"00000000";
+    signal counter : signed(31 downto 0);
     
-    
+    -- Threshold
+	signal threshold : signed(31 downto 0);
+	-- Phase contribution
+	signal phase_contribution : signed(31 downto 0);
 begin
 
+-- Set the counting max to the clock frequency/2
+COUNT_MAX(31 downto 1) <= clk_freq(30 downto 0);
 
-with freq select
-    counter_amount <=   x"00000010" when "0000",
-                        x"00000002" when "0001",
-                        x"00000001" when others;
+-- Find thresholds
+threshold_high <= clk_freq * duty_cycle;
+threshold_low  <= threshold_high - DEADTIME;
+
+
 
 
 -- Counts on every clock pulse
@@ -57,9 +65,9 @@ counter_process:
 process (clk)
 begin
    if rising_edge(clk) then
-        if dir = UP then
+		if dir = UP then
             -- Count up
-            counter <= counter + counter_amount;
+            counter <= counter + 1;
             
             -- If max is reached. Change direction
             if counter >= COUNT_MAX then
@@ -67,10 +75,13 @@ begin
             end if;
         elsif dir = DOWN then
             -- Count down
-            counter <= counter - counter_amount;
+            counter <= counter - 1;
             
             -- If min is reached. Change direction
             if counter <= COUNT_MIN then
+				-- Phase contribution
+				phase_contribution <= ( phase * 120 ) * COUNT_MAX;
+				
                 dir <= UP ;
             end if;
         else
